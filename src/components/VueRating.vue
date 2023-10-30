7e7ecb01c8cf82f38be306fc3aa4045dbd08c545
achraffawzi <achraffawzi224a@gmail.com>
2023-10-30 17:17:06 +0100

    Fill stars on mouseover

--- src/components/VueRating.vue
+++ src/components/VueRating.vue
@@ -7,8 +7,10 @@
       width="23"
       height="23"
       viewBox="0 0 100 100"
+      @mouseover="handleMouseOver(item)"
     >
       <polygon
+        ref="polygonRef"
         points="50,0 64,36 100,42 72,64 78,100 50,80 22,100 28,64 0,42 36,36"
         fill="none"
         stroke="#000"
@@ -43,6 +45,28 @@ const props = defineProps({
     default: false,
   },
 });
+
+const polygonRef = ref();
+
+const fill = (from, to) => {
+  for (let i = from; i < to; i++) {
+    polygonRef.value[i].style.fill = props.fill;
+  }
+};
+
+const unfill = (from, to) => {
+  console.log(from);
+  console.log(to);
+  for (let i = from; i < to; i++) {
+    polygonRef.value[i].style.fill = "none";
+  }
+};
+
+const handleMouseOver = (index) => {
+  if (props.disabled || props.readOnly) return;
+  fill(0, index);
+  unfill(index, props.length);
+};
 </script>
 
 <style scoped>
